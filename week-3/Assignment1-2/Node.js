a716fbb72238e231c84918fd088ab222727e1661
Sandy-Dai <sandy13dai@gmail.com>
2023-01-20 11:20:14 +0800

    Assignment1-2

---
+++ week-3/Assignment1-2/Node.js
@@ -0,0 +1,38 @@
+const express =require('express');
+const app = express();  //express application 物件
+
+const port = 3000;
+
+app.set('view engine', 'pug'); // 套入pug
+
+
+app.get('/',function(req, res){ //API設定
+        res.render('index');
+});
+
+
+app.get('/getData', function(req, res){ 
+        let number = req.query.number; //型態為字串
+        let StringNum = Number(number) //轉化成數字型態
+        let sum = 0;
+        if(number == StringNum) {  //==能成功, =和===跳到23行的else？
+        for(i = 1 ; i <= Number(number) ; i++)
+        sum = sum + i;   
+        res.send(sum.toString());
+        } else if(number === 'xyz'){
+        res.send('<h1>Wrong Parameters</h1>');  
+        } else {
+        res.send('<h1>Lack of Parameter</h1>');
+        }
+        });
+
+
+        
+
+
+
+
+app.listen(port, function(){ //終端機輸入node Node.js 會出現以下字串
+         console.log(`Now listening on localhost:${port}!`); 
+});
+
---
+++ week-3/Node.js
@@ -0,0 +1,11 @@
+const express =require('express');
+const app = express();
+const port = 3000;
+
+app.get('/',function(req, res){
+        res.sendFile('index.html', {root: __dirname});
+});
+
+app.listen(port, function(){
+         console.log(`Now listening on port ${port}`); 
+});
