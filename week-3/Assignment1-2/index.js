dcc0b534305e5c4a2bccb82a8e8075f3eaa6e54a
Sandy-Dai <sandy13dai@gmail.com>
2023-01-22 16:16:53 +0800

    Renew Assignment1-3

--- week-3/Assignment1-2/index.js
+++ week-3/Assignment1-2/index.js
@@ -3,26 +3,39 @@ const app = express();  //express application 物件
 
 const port = 3000;
 
+app.use(express.static('public'));
+app.use(express.static('files'));
+
 app.set('view engine', 'pug'); // 套入pug
 
+// app.all("*", function(req,res){
+//         res.header('Access-Control-Allow-Origin','*');
+//         res.header('Access-Control-Allow-Methods','GET,POST,PUT');
+        // res.header('Access-Control-Allow-Headers', 'Content-Type');
+// })
 
 app.get('/',function(req, res){ //API設定
         res.render('index');
+        
 });
 
 
 app.get('/getData', function(req, res){ 
-        let number = req.query.number; //型態為字串
-        let stringNum = Number(number) //轉化成數字型態
+        res.setHeader('Access-Control-Allow-Origin','*')        //設定回應頭，設置允許跨域（要設定在最上方，程式由上往下跑）
+
+        let number = req.query.number; //型態為字串    
+        let stringNum = Number(number) //轉化成數字型態 
         let sum = 0;
-        if(number == stringNum) {  //==能成功, =和===跳到第24行的else？
-        for(i = 1 ; i <= stringNum ; i++)
-        sum = sum + i;   
-        res.send(sum.toString());
+
+        if(number == stringNum ) {  //==能成功, =和===跳到第24行的else？
+                for(i = 1 ; i <= stringNum ; i++)
+                sum = sum + i; 
+                //res.setHeader('Access-Control-Allow-Origin','*')  //設定回應頭，設置允許跨域
+                res.send(sum.toString()); 
         } else if(number === 'xyz'){
-        res.send('<h1>Wrong Parameters</h1>');  
+                res.send('<h1>Wrong Parameters</h1>');  
         } else {
-        res.send('<h1>Lack of Parameter</h1>');
+                res.send('<h1>Lack of Parameter</h1>');
         }
         });
 
