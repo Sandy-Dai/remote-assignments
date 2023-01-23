d063e5203d55bba8b5112b9f16f36b757c56186e
Sandy-Dai <sandy13dai@gmail.com>
2023-01-23 21:34:09 +0800

    Revise Wrong&Lack of Parameter

--- week-3/Assignment1-2/index.js
+++ week-3/Assignment1-2/index.js
@@ -1,4 +1,4 @@
-const express =require('express');
+const express = require('express');
 const app = express();  //express application 物件
 
 const port = 3000;
@@ -19,26 +19,28 @@ app.get('/',function(req, res){ //API設定
         
 });
 
+// app.get('/getData', function(req, res){ 
+//       res.send('<h1>Lack of Parameter</h1>');
+//         }
+        
+
 
 app.get('/getData', function(req, res){ 
         res.setHeader('Access-Control-Allow-Origin','*')        //設定回應頭，設置允許跨域（要設定在最上方，程式由上往下跑）
-
-        let number = req.query.number; //型態為字串    
-        let stringNum = Number(number) //轉化成數字型態 
+        let value = req.query.number; //型態為字串    
+        let stringNum = Number(value) //轉化成數字型態 
         let sum = 0;
-
-        if(number == stringNum ) {  //==能成功, =和===跳到第24行的else？
+        
+        if(value == stringNum ) {  //判斷型態是否為數字
                 for(i = 1 ; i <= stringNum ; i++)
                 sum = sum + i; 
-                //res.setHeader('Access-Control-Allow-Origin','*')  //設定回應頭，設置允許跨域
                 res.send(sum.toString()); 
-        } else if(number === 'xyz'){
+        } else if(typeof value === 'string'){ //判斷型態是否為字串
                 res.send('<h1>Wrong Parameters</h1>');  
-        } else {
+        } else if(typeof value === 'undefined'){
                 res.send('<h1>Lack of Parameter</h1>');
-        }
-        });
-
+        };
+});
 
 
 
@@ -47,5 +49,6 @@ app.get('/getData', function(req, res){
 
 app.listen(port, function(){ //終端機輸入node Node.js 會出現以下字串
          console.log(`Now listening on localhost:${port}!`); 
-});
+})
+
 
