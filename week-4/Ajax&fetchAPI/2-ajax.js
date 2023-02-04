ff1a5522a908c7958aa185b32980eeeb02870d23
Sandy-Dai <sandy13dai@gmail.com>
2023-02-05 01:15:34 +0800

    prettier

--- week-4/Ajax&fetchAPI/2-ajax.js
+++ week-4/Ajax&fetchAPI/2-ajax.js
@@ -1,42 +1,36 @@
 function ajax(url) {
-    const promise = fetch("https://appworks-school.github.io/Remote-Assignment-Data/products")
-        .then(response => {
-            if(!response.ok) {
-                throw Error("Error");
-            }
-            return response.json();
-        }); 
-        return promise;
-};
-
-
-
-
+  const promise = fetch(
+    "https://appworks-school.github.io/Remote-Assignment-Data/products"
+  ).then((response) => {
+    if (!response.ok) {
+      throw Error("Error");
+    }
+    return response.json();
+  });
+  return promise;
+}
 
 function render(data) {
-
-        console.log(data);
-        const html = data.map(product => {
-                return `
+  console.log(data);
+  const html = data
+    .map((product) => {
+      return `
                 <div class="product">
                 <p>Name: ${product.name}</p>
                 <p>Price: ${product.price}</p>
                 <p>Description: ${product.description}</p>
                 </div> `;
-            }).join(""); //讓console.log(html)的字串連一起
-            console.log(html);
-            
-        document
-            .querySelector("#items") //連動html裡的id
-            .insertAdjacentHTML('afterbegin', html); //加入獲取的html字串 
+    })
+    .join(""); //讓console.log(html)的字串連一起
+  console.log(html);
 
-
-};
+  document
+    .querySelector("#items") //連動html裡的id
+    .insertAdjacentHTML("afterbegin", html); //加入獲取的html字串
+}
 
 const url = "https://appworks-school.github.io/Remote-Assignment-Data/products";
 
-
 ajax(url).then((data) => {
-    render(data);
+  render(data);
 });
-
--- week-4/callback-function/callbackFunction.js
+++ week-4/callback-function/callbackFunction.js
@@ -1,23 +1,24 @@
-function delayedResult(n1,n2,delayTime,callback){
-    const sum = n1+n2 + `(${n1} + ${n2})`;
-    
-    setTimeout(() => {
-        if (delayTime >= 3000){
-            console.log(sum);
-        }else {
-            window.alert(sum);
-        };
-    },delayTime);
+function delayedResult(n1, n2, delayTime, callback) {
+  const sum = n1 + n2 + `(${n1} + ${n2})`;
 
-};
+  setTimeout(() => {
+    if (delayTime >= 3000) {
+      // console.log(sum);
+      callback(sum);
+    } else {
+      // window.alert(sum);
+      callback(sum);
+    }
+  }, delayTime);
+}
 
-delayedResult(4,5,3000,function(result){
-    console.log(result); 
+delayedResult(4, 5, 3000, function (result) {
+  console.log(result);
 }); //9(4+5) will be shown in the console after 3 seconds
 
-delayedResult(-5,10,2000,function(result){
-    window.alert(result);
-}); //5(-5+10) will be shown in an alert dialog after 2 seconds 
+delayedResult(-5, 10, 2000, function (result) {
+  window.alert(result);
+}); //5(-5+10) will be shown in an alert dialog after 2 seconds
 
 //console.log 9(4+5) 3000 ,5
 //window alert 9(4+5) 2000 ,9
@@ -26,5 +27,3 @@ delayedResult(-5,10,2000,function(result){
 //console.log 5(-5+10) 3000 ,5
 //window alert 5(-5+10) 2000 ,9
 //undefined ,19
-
-
